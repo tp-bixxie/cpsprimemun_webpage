37db96d15b2afd43167c26777018db67e7a62908
Tharun Pranav S V <35001651+tp-bixxie@users.noreply.github.com>
2023-06-05 19:11:10 +0530

    Update 2 with dist

--- cpsprimemun_webapp/registration/components/index.js
+++
@@ -1,9 +0,0 @@
-import Banner from "./Banner";
-import Content from "./Content";
-import Map from "./Map";
-
-export {
-    Banner,
-    Content,
-    Map,
-}
--- cpsprimemun_webapp/vite.config.js
+++ cpsprimemun_webapp/vite.config.js
@@ -6,26 +6,26 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/cpsprimemun_webpage/",
   plugins: [
     react(), 
           ],
   build: {
     rollupOptions: {
       input: {
-        home: resolve(__dirname, 'index.html'),
-        about: resolve(__dirname, '/about/index.html'),
-        committee: resolve (__dirname, '/committee/index.html'),
-        conference: resolve (__dirname, '/conference/index.html'),
-        registration: resolve (__dirname, '/registration/index.html'),
-        WHO: resolve (__dirname, '/WHO/index.html'),
-        ECOSOC: resolve (__dirname, '/EC0S0C/index.html'),
-        G20: resolve (__dirname, '/G20/index.html'),
-        IP: resolve (__dirname, '/IP/index.html'),
-        UNCSW: resolve (__dirname, '/UNCSW/index.html'),
-        UNFCCC: resolve (__dirname, '/UNFCCC/index.html'),
-        UNGA: resolve (__dirname, '/UNGA/index.html'),
-        UNHRC: resolve (__dirname, '/UNHRC/index.html'),
-        UNSC: resolve (__dirname, '/UNSC/index.html'),
+        main: resolve(__dirname, './index.html'),
+        about: resolve(__dirname, './about/index.html'),
+        committee: resolve (__dirname, './committee/index.html'),
+        conference: resolve (__dirname, './conference/index.html'),
+        WHO: resolve (__dirname, './WHO/index.html'),
+        ECOSOC: resolve (__dirname, './ECOSOC/index.html'),
+        G20: resolve (__dirname, './G20/index.html'),
+        IP: resolve (__dirname, './IP/index.html'),
+        UNCSW: resolve (__dirname, './UNCSW/index.html'),
+        UNFCCC: resolve (__dirname, './UNFCCC/index.html'),
+        UNGA: resolve (__dirname, './UNGA/index.html'),
+        UNHRC: resolve (__dirname, './UNHRC/index.html'),
+        UNSC: resolve (__dirname, './UNSC/index.html'),
 
       },
     },
